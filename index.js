c31e8e29e50eddef169101da53d495c4fd16fd1e
charile1 <pyo05360@gmail.com>
2021-11-28 21:22:57 +0900

    js

---
+++ index.js
@@ -0,0 +1,14 @@
+// const arr = [1, 2, 3];
+
+// arr.forEach(console.log);
+
+// const arr = [1, 2, 3];
+
+// arr.forEach(alert);
+
+console.log(score);
+
+var score;
+score = 80;
+
+console.log(score);
